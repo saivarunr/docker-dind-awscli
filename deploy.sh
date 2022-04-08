a38f203a4e9ea4c3dda38e81aa2feea01f2087d5
Sai Varun Reddy <saivarunvishal@gmail.com>
2022-04-08 10:57:09 +0530

    1. Uses arguments from runtime

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cluster_name=$1
 service_name=$2
-TASK_DEF_FAMILY_NAME=$3
-NEW_IMAGE=$4
+task_def_family_name=$3
+new_image=$4
 TASK_ARN=$(aws ecs describe-services --service $service_name --cluster $cluster_name | jq ".services[].taskDefinition" | sed -e 's/^"//' -e 's/"$//')
 aws ecs describe-task-definition --task-definition $TASK_ARN>describe-task.json
-sed -i -e "s|\"image\".*|\"image\":\"$NEW_IMAGE\"\,|g" describe-task.json
+sed -i -e "s|\"image\".*|\"image\":\"$new_image\"\,|g" describe-task.json
 CONTAINER_DEFS=$(cat describe-task.json | jq ".taskDefinition.containerDefinitions")
 echo "{\"containerDefinitions\":$CONTAINER_DEFS}" | cat>container-def.json
-NEW_TASK_DEF_VERSION=$(aws ecs register-task-definition --family $TASK_DEF_FAMILY_NAME --cli-input-json file://container-def.json | jq ".taskDefinition.revision")
-aws ecs update-service --cluster $CLUSTER_NAME --service $SERVICE_NAME --task-definition "$TASK_DEF_FAMILY_NAME:$NEW_TASK_DEF_VERSION" --force-new-deployment
+NEW_TASK_DEF_VERSION=$(aws ecs register-task-definition --family $task_def_family_name --cli-input-json file://container-def.json | jq ".taskDefinition.revision")
+aws ecs update-service --cluster $cluster_name --service $service_name --task-definition "$task_def_family_name:$NEW_TASK_DEF_VERSION" --force-new-deployment
