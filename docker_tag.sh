aa78548664a9bf02b587cfd52a8cefa1d632c9f8
Sai Varun Reddy <saivarunvishal@gmail.com>
2022-04-08 17:59:35 +0530

    Uses CI_MERGE_REQUEST_TARGET_BRANCH_NAME

--- docker_tag.sh
+++ docker_tag.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 semver_file_name=$1
 release_version=v`cat $semver_file_name | grep '^\s*\"*version' | grep -oE "\d*\.\d*\.\d*"`
-if [[ "$CI_COMMIT_BRANCH" != 'main' && "$CI_COMMIT_BRANCH" != 'master' ]]; then release_version="$release_version-$CI_COMMIT_BRANCH"; fi;
+if [[ "$CI_MERGE_REQUEST_TARGET_BRANCH_NAME" != 'main' && "$CI_MERGE_REQUEST_TARGET_BRANCH_NAME" != 'master' ]]; then release_version="$release_version-$CI_MERGE_REQUEST_TARGET_BRANCH_NAME"; fi;
 echo "$release_version"
